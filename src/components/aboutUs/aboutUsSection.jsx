de6de92d5aa1d9ccf7121bf904e747ab8bf4f9bf
AliJanum1214 <shampi.goli@gmail.com>
2025-05-27 20:40:07 +0500

    commit

--- src/components/aboutUs/aboutUsSection.jsx
+++ src/components/aboutUs/aboutUsSection.jsx
@@ -12,17 +12,17 @@ export default function AboutUsSection() {
     {
       heading: "Our Story",
       title: "From Karachi to London",
-      text: `In 2001, Asghar Khan arrived in London with a dream: to bring the vibrant flavors of his beloved Burns Road to his new home...`,
+      text: `In 2001, Asghar Khan arrived in London with a dream: to bring In the bustling streets of Karachi, Dilpasand began as a humble eatery in 1949, serving soulful dishes that blended the vibrant flavors of South Asia with the warmth of home. Founded by a family passionate about food and community, it quickly became a beloved gathering spot where stories were shared over plates of aromatic biryani and delicate sweets.Decades later, this legacy crossed oceans to London, where Dilpasand Restaurant now stands as a beacon of heritage and hospitality. Here, we continue to honor our roots, offering a menu that tells the story of Karachi’s bustling markets and London’s cosmopolitan charm, inviting guests to savor a taste of history with every bite.`,
     },
     {
       heading: "Our Growth",
       title: "More Than Just a Restaurant",
-      text: `Dilpasand quickly became a beloved destination, known not just for its food but for its spirit...`,
+      text: `In 2001, Asghar Khan arrived in London with a dream: to bring In the bustling streets of Karachi, Dilpasand began as a humble eatery in 1949, serving soulful dishes that blended the vibrant flavors of South Asia with the warmth of home. Founded by a family passionate about food and community, it quickly became a beloved gathering spot where stories were shared over plates of aromatic biryani and delicate sweets. Decades later, this legacy crossed oceans to London, where Dilpasand Restaurant now stands as a beacon of heritage and hospitality. Here, we continue to honor our roots, offering a menu that tells the story of Karachi’s bustling markets and London’s cosmopolitan charm, inviting guests to savor a taste of history with every bite.`,
     },
     {
       heading: "Today & Beyond",
       title: "Carrying the Flame Forward",
-      text: `Now managed by the next generation, Dilpasand continues to bring joy to food lovers...`,
+      text: `In 2001, Asghar Khan arrived in London with a dream: to bring In the bustling streets of Karachi, Dilpasand began as a humble eatery in 1949, serving soulful dishes that blended the vibrant flavors of South Asia with the warmth of home. Founded by a family passionate about food and community, it quickly became a beloved gathering spot where stories were shared over plates of aromatic biryani and delicate sweets. Decades later, this legacy crossed oceans to London, where Dilpasand Restaurant now stands as a beacon of heritage and hospitality. Here, we continue to honor our roots, offering a menu that tells the story of Karachi’s bustling markets and London’s cosmopolitan charm, inviting guests to savor a taste of history with every bite.`,
     },
   ];
 
@@ -35,7 +35,7 @@ export default function AboutUsSection() {
 
   useEffect(() => {
     const handleScroll = () => {
-      const triggerPoint = window.innerHeight * 2; // 140vh
+      const triggerPoint = window.innerHeight * 2;
       if (window.scrollY >= triggerPoint) {
         setShowMenu(true);
       } else {
@@ -49,7 +49,7 @@ export default function AboutUsSection() {
 
   return (
     <>
-      <div className="bg-custom-primary text-white relative overflow-hidden py-20 md:px-0 px-6">
+      <div className="bg-custom-primary text-white relative overflow-hidden border border-white md:px-0 px-6 h-screen flex justify-center items-center">
         <div className="flex flex-col md:flex-row justify-between pt-16 items-center gap-10 max-w-6xl mx-auto border custom-border p-8">
           {/* Left */}
           <div className="md:w-[30%] w-full flex items-center gap-4">
@@ -74,13 +74,13 @@ export default function AboutUsSection() {
 
           {/* Right */}
           <div className="md:w-[70%] w-full flex flex-col justify-center items-start gap-4 p-4">
-            <h5 className="text-2xl font-[400] text-white">
+            <h5 className="text-2xl font-[400] text-custom-secondary">
               {content[step].heading}
             </h5>
-            <h3 className="text-custom-secondary text-4xl font-[500]">
+            <h3 className="text-yellow-500 text-4xl font-[500]">
               {content[step].title}
             </h3>
-            <p>{content[step].text}</p>
+            <p className="text-white">{content[step].text}</p>
             <button className="relative group border-none text-white outline-none cursor-pointer mt-2 p-1 capitalize flex gap-3 items-center">
               Read more about the story{" "}
               <span>
