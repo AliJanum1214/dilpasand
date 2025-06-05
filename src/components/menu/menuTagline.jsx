14631076463d5229f5109a83d083909795273f8c
AliJanum1214 <shampi.goli@gmail.com>
2025-06-05 12:23:53 +0500

    commit

--- src/components/menu/menuTagline.jsx
+++ src/components/menu/menuTagline.jsx
@@ -1,13 +1,3 @@
 export default function MenuTagline() {
-  return (
-    <div className="bg-transparent text-white py-6 px-4 flex items-center justify-center">
-      <div className="flex items-center w-full sm:max-w-xl md:max-w-4xl max-[1280px]:max-w-4xl max-[1330px]:max-w-6xl max-[1440px]:max-w-7xl">
-        <div className="flex-grow border-t-2 custom-border" />
-        <h4 className="px-4 text-lg md:text-[20px] font-bold italic text-white whitespace-nowrap">
-          Explore Full Menu
-        </h4>
-        <div className="flex-grow border-t-2 custom-border" />
-      </div>
-    </div>
-  );
+  return <></>;
 }
