a8906101e1fe5ee2c777fbf95a297c8806ef5533
AliJanum1214 <shampi.goli@gmail.com>
2025-06-05 15:04:42 +0500

    commit

--- src/components/reservation/private-dining/privateDiningSection.jsx
+++ src/components/reservation/private-dining/privateDiningSection.jsx
@@ -1,8 +1,6 @@
 "use client";
 import React from "react";
 import ReservationForm from "./ReservationForm";
-import GroupBookingInfo from "./GroupBookingInfo";
-import { Box } from "@mui/material";
 
 const PrivateDiningSection = () => {
   return (
